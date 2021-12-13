85f047a9f3bcc0b4e4735217b0754ff94f91e816
Jason Mullings <wjasonmullings@gmail.com>
2021-12-13 11:24:08 -0500

    added cartIcon and cartDropdown and cartReducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,16 +5,18 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 //higher order component that lets us modify our components with redux
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 
 import './header.styles.scss';
 
 
-const Header = ({ currentUser }) => (
-   
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
       <Link to='/' className='logo-container' >
         <Logo className='logo'/>
-      </Link>
+    </Link>
       <div className='options'>
         <Link to='/shop' className='option'>
           SHOP
@@ -22,19 +24,28 @@ const Header = ({ currentUser }) => (
         <Link to='/shop' className='option'>
         CONTACT
         </Link>
-        {
-          currentUser && currentUser.userAuth !== null  ?
-            <div className='option' onClick={()=> auth.signOut()}> SIGN OUT</div>
-            :
-            <Link className="option" to='/signin'>SIGN IN</Link>
-        }
-      </div>
+      {
+        currentUser && currentUser.userAuth !== null ?
+          <div className='option' onClick={() => auth.signOut()}> SIGN OUT</div>
+          :
+          <Link className="option" to='/signin'>SIGN IN</Link>
+      }
+      <CartIcon/>
+    </div>
+    {
+      hidden ?
+        null
+        :
+      <CartDropdown />
+    }
     </div>
   );
 
 //this function gets the state from our redux
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
+//below I destruct the nested props, getting the currentUser value and hidden value directly
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+  currentUser,
+  hidden
 });
 
   
